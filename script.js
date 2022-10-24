618eab68f6603792b714ec3aa57ed2972601f504
bel4enok03 <belskaya-tanyush@mail.ru>
2022-10-24 21:35:32 +0400

    *

--- script.js
+++ script.js
@@ -4,128 +4,64 @@ const buttonHard = document.querySelector('#btn-hard');
 
 const countdown = document.querySelector('#countdown');
 
+let timerId = null;
+let amountTime = null;
+
 buttonSoft.addEventListener('click', function () {
-	document.querySelector('#medium').style.display = 'none';
-	document.querySelector('#soft').style.display = 'block';
-	document.querySelector('#hard').style.display = 'none';
-
-	const timer = 3;
-	let amountTime = timer * 60;
-
-	function calcilateTimerSoft() {
-		let minutes = Math.floor(amountTime / 60);
-		let seconds = amountTime % 60;
-
-		if (seconds < 10) {
-			seconds = '0' + seconds;
-		}
-
-		countdown.textContent = `${minutes} : ${seconds}`;
-		amountTime--;
-
-		if (amountTime < 0) {
-			stopTimer();
-			amountTime = 0;
-		}
-
-		if (
-			(document.querySelector('#btn-medium').onclick = function () {
-				stopTimer();
-			})
-		)
-			if (
-				(document.querySelector('#btn-hard').onclick = function () {
-					stopTimer();
-				})
-			)
-				function stopTimer() {
-					clearInterval(timerId);
-				}
-	}
-
-	let timerId = setInterval(calcilateTimerSoft, 1000);
+    process('soft')
 });
 
 buttonMedium.addEventListener('click', function () {
-	document.querySelector('#medium').style.display = 'block';
-	document.querySelector('#soft').style.display = 'none';
-	document.querySelector('#hard').style.display = 'none';
-
-	const timer = 5;
-	let amountTime = timer * 60;
-
-	function calcilateTimerMedium() {
-		let minutes = Math.floor(amountTime / 60);
-		let seconds = amountTime % 60;
-
-		if (seconds < 10) {
-			seconds = '0' + seconds;
-		}
-
-		countdown.textContent = `${minutes} : ${seconds}`;
-		amountTime--;
-
-		if (amountTime < 0) {
-			stopTimer();
-			amountTime = 0;
-		}
-
-		if (
-			(document.querySelector('#btn-soft').onclick = function () {
-				stopTimer();
-			})
-		)
-			if (
-				(document.querySelector('#btn-hard').onclick = function () {
-					stopTimer();
-				})
-			)
-				function stopTimer() {
-					clearInterval(timerId);
-				}
-	}
-
-	let timerId = setInterval(calcilateTimerMedium, 1000);
+    process('medium')
 });
 
 buttonHard.addEventListener('click', function () {
-	document.querySelector('#medium').style.display = 'block';
-	document.querySelector('#soft').style.display = 'none';
-	document.querySelector('#hard').style.display = 'none';
-
-	const timer = 9;
-	let amountTime = timer * 60;
-
-	function calcilateTimerHard() {
-		let minutes = Math.floor(amountTime / 60);
-		let seconds = amountTime % 60;
-
-		if (seconds < 10) {
-			seconds = '0' + seconds;
-		}
-
-		countdown.textContent = `${minutes} : ${seconds}`;
-		amountTime--;
-
-		if (amountTime < 0) {
-			stopTimer();
-			amountTime = 0;
-		}
-
-		if (
-			(document.querySelector('#btn-soft').onclick = function () {
-				stopTimer();
-			})
-		)
-			if (
-				(document.querySelector('#btn-medium').onclick = function () {
-					stopTimer();
-				})
-			)
-				function stopTimer() {
-					clearInterval(timerId);
-				}
-	}
-
-	let timerId = setInterval(calcilateTimerHard, 1000);
+    process('hard')
 });
+
+function process(btn) {
+    if (timerId != null) {
+        timerId = stopTimer();
+    }
+
+    changeBtnCapture(btn);
+
+    let time;
+    switch (btn) {
+        case 'soft': time = 3; break;
+        case 'medium': time = 5; break;
+        case 'hard': time = 9; break;
+        default: alert( "Нет таких значений");
+    }
+
+    amountTime = time * 60;
+
+	calcilateTimer();
+    
+	timerId = setInterval(calcilateTimer, 1000);
+}
+
+function changeBtnCapture(btn) {
+    document.querySelector('#medium').style.display = btn == 'medium' ? 'block' : 'none' ;
+	document.querySelector('#soft').style.display = btn == 'soft' ? 'block' : 'none' ;
+	document.querySelector('#hard').style.display = btn == 'hard' ? 'block' : 'none' ;
+}
+
+function calcilateTimer() {
+    if(amountTime <= 0){
+        stopTimer();
+    }
+    let minutes = Math.floor(amountTime / 60);
+    let seconds = amountTime % 60;
+
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+}
+
+function stopTimer() {
+    clearInterval(timerId);
+}
